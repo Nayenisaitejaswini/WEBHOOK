c859fa0abdb4d96039721a4f47bf051a88c631c6
Sai Tejaswini <nayenisaitejaswini@gmail.com>
2026-06-04 11:42:49 +0530

    final

--- srv/service.js
+++ srv/service.js
@@ -6,52 +6,87 @@ module.exports = cds.service.impl(async function () {
 
   this.on('uploadXML', async (req) => {
     const { fileName, xmlContent } = req.data;
-
-    if (!xmlContent) return JSON.stringify({ error: "XML content is empty" });
+    if (!xmlContent) return { error: "XML content is empty" };
 
     const parser = new xml2js.Parser({ explicitArray: false, ignoreAttrs: false });
-
     try {
       const parsed = await parser.parseStringPromise(xmlContent);
+      const initn = parsed?.Document?.CstmrCdtTrfInitn;
+      if (!initn) return { error: "Invalid XML structure" };
 
-      if (!parsed?.Document?.CstmrCdtTrfInitn) {
-        console.error("Invalid XML structure:", xmlContent);
-        return JSON.stringify({ error: "Invalid XML structure" });
-      }
-
-      const initn = parsed.Document.CstmrCdtTrfInitn;
       const grpHdr = initn.GrpHdr;
       const pmtInf = initn.PmtInf;
-      const tx = Array.isArray(pmtInf.CdtTrfTxInf) ? pmtInf.CdtTrfTxInf[0] : pmtInf.CdtTrfTxInf;
+      const txs = Array.isArray(pmtInf.CdtTrfTxInf) ? pmtInf.CdtTrfTxInf : [pmtInf.CdtTrfTxInf];
 
-      const amount = typeof tx.Amt.InstdAmt === "object" ? tx.Amt.InstdAmt._ : tx.Amt.InstdAmt;
-      const currency = tx.Amt.InstdAmt?.$?.Ccy || "INR";
+      const domesticPayments = txs.map(tx => ({
+        ConsentId: "256660",
+        Initiation: {
+          InstructionIdentification: tx.PmtId?.InstrId || "",
+          EndToEndIdentification: tx.PmtId?.EndToEndId || "",
+          ClearingSystemIdentification: tx.ClearingSystemIdentification || "NEFT",
+          InstructedAmount: {
+            Amount: tx.Amt?.InstdAmt?._ || tx.Amt?.InstdAmt || "",
+            Currency: tx.Amt?.InstdAmt?.$?.Ccy || "INR"
+          },
+          DebtorAccount: {
+            SchemeName: tx.DbtrAcct?.Id?.Othr?.SchmeNm?.Cd || "",
+            Identification: tx.DbtrAcct?.Id?.Othr?.Id || "",
+            Name: tx.DbtrAcct?.Nm || "Unknown",
+            SecondaryIdentification: tx.DbtrAcct?.Othr?.Id || "",
+            Unstructured: {
+              ContactInformation: {
+                MobileNumber: tx.DbtrAcct?.CtctDtls?.MobNb || ""
+              },
+              Identities: {
+                MobileNumber: tx.DbtrAcct?.CtctDtls?.MobNb || ""
+              }
+            }
+          },
+          CreditorAccount: {
+            SchemeName: tx.CdtrAcct?.Id?.Othr?.SchmeNm?.Cd || "",
+            Identification: tx.CdtrAcct?.Id?.Othr?.Id || "",
+            Name: tx.Cdtr?.Nm || "",
+            Unstructured: {
+              ContactInformation: {
+                EmailAddress: tx.Cdtr?.CtctDtls?.EmailAdr || "",
+                MobileNumber: tx.Cdtr?.CtctDtls?.MobNb || ""
+              },
+              Identities: {}
+            }
+          },
+          RemittanceInformation: {
+            Unstructured: {
+              CreditorReferenceInformation: tx.RmtInf?.Strd?.RfrdDocInf?.NbText || "",
+              RemitterAccount: tx.DbtrAcct?.Id?.Othr?.Id || ""
+            }
+          }
+        },
+        Risk: {
+          PaymentContextCode: "BankTransfer",
+          DeliveryAddress: {
+            Country: tx.CdtrAgt?.FinInstnId?.PstlAdr?.Ctry || "IN",
+            AddressLine: Array.isArray(tx.CdtrAgt?.FinInstnId?.PstlAdr?.AdrLine)
+              ? tx.CdtrAgt.FinInstnId.PstlAdr.AdrLine
+              : [tx.CdtrAgt?.FinInstnId?.PstlAdr?.AdrLine].filter(Boolean)
+          }
+        }
+      }));
 
       const yesBankPayload = {
         Data: {
           FileIdentifier: grpHdr?.MsgId || "",
-          NumberOfTransactions: grpHdr?.NbOfTxs || "0",
+          NumberOfTransactions: grpHdr?.NbOfTxs || txs.length,
           ConsentId: "256660",
-          ControSum: grpHdr?.CtrlSum || "0",
+          ControSum: grpHdr?.CtrlSum || "",
           SecondaryIdentification: "256660",
-          DomesticPayments: [
-            {
-              ConsentId: "256660",
-              Initiation: {
-                InstructionIdentification: tx?.PmtId?.InstrId || "",
-                EndToEndIdentification: tx?.PmtId?.EndToEndId || "",
-                ClearingSystemIdentification: "NEFT",
-                InstructedAmount: { Amount: amount, Currency: currency }
-              }
-            }
-          ]
+          DomesticPayments: domesticPayments
         }
       };
 
       await INSERT.into(PaymentFiles).entries({
         ID: cds.utils.uuid(),
         fileIdentifier: grpHdr?.MsgId || "",
-        numberOfTransactions: Number(grpHdr?.NbOfTxs || 0),
+        numberOfTransactions: Number(grpHdr?.NbOfTxs || txs.length),
         consentId: "256660",
         controlSum: Number(grpHdr?.CtrlSum || 0),
         secondaryIdentification: "256660",
@@ -67,7 +102,7 @@ module.exports = cds.service.impl(async function () {
 
     } catch (err) {
       console.error("XML parsing error:", err);
-      return JSON.stringify({ error: "Failed to parse XML" });
+      return { error: "Failed to parse XML" };
     }
   });
 });
